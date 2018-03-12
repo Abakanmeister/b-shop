6b56eb9689f487f2b1a4ee40fa5c2eff336262cb
Unknown <tuguzhekov95@mail.ru>
2018-03-12 13:32:51 +0700

    same

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,4 @@
 $(function () {
-    $('form.create-catalog-form').on('submit', function () {
-        var csrfName = $('#csrf_name').val();
-        var csrfValue =$('#csrf_value').val();
-        var csrfData = {"csrf_name" : csrfName,"csrf_value" : csrfValue};
-        $.ajax({
-            url : "/api/catalog/add",
-            type: "POST",
-            data : csrfData,
-            success: function(data, textStatus, jqXHR)
-            {
-                console.log('SUCCESS');
-            },
-            error: function (jqXHR, textStatus, errorThrown)
-            {
-
-            }
-        });
-    });
-
     $('form.create-product-form').on('submit', function () {
         var catalog_id = $('select[car-brand] option').val();
         console.log(catalog_id);
@@ -33,6 +14,7 @@ $(function () {
         $('th', this).html(i);
         i++;
     });
+
 });
 
 
